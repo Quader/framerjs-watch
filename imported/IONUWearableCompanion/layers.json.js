716b7a6a7618336659a0860158c8a3fb6b1c60c2
Steven David <steven.david@webvariants.de>
2014-07-31 23:53:12 +0200

    Added github page

---
+++ imported/IONUWearableCompanion/layers.json.js
@@ -0,0 +1,53 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["IONUWearableCompanion/layers.json.js"] = [
+	{
+		"id": 40,
+		"name": "designer-front",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 1920,
+			"height": 1080
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/designer-front.png",
+			"frame": {
+				"x": 0,
+				"y": 0,
+				"width": 1920,
+				"height": 1080
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "933391548"
+	},
+	{
+		"id": 69,
+		"name": "CC",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 1920,
+			"height": 1080
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/CC.png",
+			"frame": {
+				"x": 0,
+				"y": 0,
+				"width": 1920,
+				"height": 1080
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "218013542"
+	}
+]
